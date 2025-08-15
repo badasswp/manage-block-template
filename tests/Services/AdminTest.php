5af53ff80bb03c99d953792b8773c7f8ddee48aa
badasswp <badasswpdev@gmail.com>
2025-08-15 15:55:11 +0100

    test: fix cover tag

--- tests/Services/AdminTest.php
+++ tests/Services/AdminTest.php
@@ -13,7 +13,7 @@ use ManageBlockTemplate\Services\Admin;
  * @covers \ManageBlockTemplate\Services\Admin::register_options_cb
  * @covers \ManageBlockTemplate\Services\Admin::get_sections
  * @covers \ManageBlockTemplate\Services\Admin::get_options
- * @covers \ManageBlockTemplate\Services\Admin::template_cb_post
+ * @covers \ManageBlockTemplate\Services\Admin::__call
  * @covers \ManageBlockTemplate\Services\Admin::sanitize_options
  * @covers \ManageBlockTemplate\Services\Admin::get_allowed_post_types
  * @covers \ManageBlockTemplate\Services\Admin::get_callback_name
