fefa7b322a75fdcbb7c70eec5228cb5ace6d523a
badasswp <badasswpdev@gmail.com>
2026-01-05 05:23:17 +0000

    push to master-built

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/manage-block-template',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'd3b3d9742442a5a0147eaa2131e61e7a67d23b34',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/manage-block-template' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'd3b3d9742442a5a0147eaa2131e61e7a67d23b34',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
