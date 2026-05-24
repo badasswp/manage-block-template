5085936c7223b3dd054b9bed8f77c5542ce74660
badasswp <badasswpdev@gmail.com>
2026-05-24 16:09:05 +0000

    Build from release-1.1.0

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'badasswp/manage-block-template',
+        'pretty_version' => 'dev-release-1.1.0-built',
+        'version' => 'dev-release-1.1.0-built',
+        'reference' => '5e1c050b2cad8359142364c6b627069a8b366233',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'badasswp/manage-block-template' => array(
+            'pretty_version' => 'dev-release-1.1.0-built',
+            'version' => 'dev-release-1.1.0-built',
+            'reference' => '5e1c050b2cad8359142364c6b627069a8b366233',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
